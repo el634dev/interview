d4ccaf6a6e235b44c63df77bc67e61565a2bd8c0
el634dev <84924260+el634dev@users.noreply.github.com>
2024-05-04 21:12:20 -0400

    Update subsequence.py

--- easy/subsequence.py
+++ easy/subsequence.py
@@ -42,52 +42,54 @@ Output: false
 # Two Pointers
 def isSubsequence(self, s: str, t: str) -> bool:
     """
-    Time Complexity: The algorithm traverses the string t once, resulting in a time complexity of O(len(t))
-    Space Complexity: Constant space is used, leading to a space complexity of O(1)O(1)O(1)
+    Time Complexity: O(n) -> The algorithm traverses the string t once, resulting in a time complexity of O(t)
+    Space Complexity: O(1) -> Constant space is used, leading to a space complexity of O(1)
     """
-    i, j = 0, 0
-    while i < len(s) and j < len(t):
-        if s[i] == t[j]:
-            i += 1
-        j += 1
-    return i == len(s)
+    i, j = 0, 0 # O(1)
+    
+    while i < len(s) and j < len(t): # O(n)
+        if s[i] == t[j]: # O(1)
+            i += 1 # O(1)
+        j += 1 # O(1)
+    return i == len(s) # O(1)
 
 # ---------------------
 # Dynamic Programming
 def is_subsequence(self, s: str, t: str) -> bool:
     """
-    Time Complexity: The algorithm traverses the string s once, resulting in a time complexity of O(len(s))
-    Space Complexity: The algorithm creates a list of dictionaries nxt of size len(t), leading to a space complexity of O(len(t))
+    Time Complexity: O(n) -> The algorithm traverses the string s once, resulting in a time complexity of O(s)
+    Space Complexity: O(n) -> The algorithm creates a list of dictionaries nxt of size len(t), leading to a space complexity of O(t)
     """
-    nxt = [{} for _ in range(len(t) + 1)]
-    for i in range(len(t) - 1, -1, -1):
-        nxt[i] = nxt[i + 1].copy()
-        nxt[i][t[i]] = i + 1
+    nxt = [{} for _ in range(len(t) + 1)] # O(1)
+    
+    for i in range(len(t) - 1, -1, -1): # O(n)
+        nxt[i] = nxt[i + 1].copy() # O(1)
+        nxt[i][t[i]] = i + 1 # O(1)
 
-    i = 0
-    for c in s:
-        if c in nxt[i]:
-            i = nxt[i][c]
-        else:
-            return False
-    return True
+    i = 0  # O(1) 
+    for c in s: # O(n)
+        if c in nxt[i]: # O(1)
+            i = nxt[i][c] # O(1)
+        else: # O(1)
+            return False # O(1)
+    return True # O(1)
 
 # ---------------------
 def is_subsequence3(self, s: str, t: str) -> bool:
     """
-    Time Complexity: O(len(t)) where t is the target string
+    Time Complexity: O(n) -> O(t) where t is the target string
     Space Complexity: O(1), constant space
     """
     # Let n and m hold the lengths of the source and target strings respectively
-    n,m = len(s),len(t)
+    n,m = len(s),len(t) # O(1)
     # i and j are the two pointers initiated as 0
-    i = j = 0
+    i = j = 0 # O(1)
 
     # While loop until pointer has reached the end for either strings traversed
-    while i < n and j < m:
+    while i < n and j < m: # O(n)
         # update both pointers if match else update only target pointer j
-        if s[i] == t[j]:
-            i += 1
-        j += 1
+        if s[i] == t[j]: # O(1)
+            i += 1 # O(1)
+        j += 1 # O(1)
     # If i is the same as source string length at the end of this, we've found a substring!
-    return i == n
+    return i == n # O(1)
