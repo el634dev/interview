9da9ca92941148a4863d4f6969364cbaba865f04
el634dev <84924260+el634dev@users.noreply.github.com>
2024-05-06 19:45:29 -0400

    Update bt_traversals.py

--- bt_traversals.py
+++ bt_traversals.py
@@ -1,18 +1,29 @@
-# All one liners
-
+# All one liners below
+# ----------------------
 # Preorder traversal
 def preorder(root):
-  return [root.val] + preorder(root.left) + preorder(root.right) if root else []
+  """
+  Time Complexity: O(1) -> Constant
+  """
+  return [root.val] + preorder(root.left) + preorder(root.right) if root else [] # O(1)
 
+# ----------------------
 # Inorder Traversal
 def inorder(root):
-  return inorder(root.left) + [root.val] + inorder(root.right) if root else []
+  """
+  Time Complexity: O(1) -> Constant
+  """
+  return inorder(root.left) + [root.val] + inorder(root.right) if root else [] # O(1)
 
+# ----------------------
 # Postorder Traversal
 def postorder(root):
-  return  postorder(root.left) + postorder(root.right) + [root.val] if root else []
+  """
+  Time Complexity: O(1) -> Constant
+  """
+  return postorder(root.left) + postorder(root.right) + [root.val] if root else [] # O(1)
 
-
+# ----------------------
 # Using a class and separate functions
 class TreeNode:
     def __init__(self, val = 0, left = None, right = None):
@@ -22,29 +33,44 @@ class TreeNode:
 
 result = []
 
+# ----------------------
+# In order traversal
 def in_order(root):
-    if not root:
-        return []
+  """
+  Time Complexity: O(1) -> Constant
+  """
+  if not root: # O(1)
+      return [] # O(1) 
 
-    in_order(root.left)
-    result.append(root.val)
-    in_order(root.right)
-    return result
+  in_order(root.left) # O(1)
+  result.append(root.val) # O(1)
+  in_order(root.right) # O(1)
+  return result # O(1)
 
+# ----------------------
+# Pre order traversal
 def pre_order(root):
-    if not root:
-        return []
+   """
+    Time Complexity: O(1) -> Constant
+   """
+  if not root:  # O(1)
+      return []  # O(1)
 
-    result.append(root.val)
-    pre_order(root.left)
-    pre_order(root.right)
-    return result
+  result.append(root.val) # O(1)
+  pre_order(root.left) # O(1)
+  pre_order(root.right) # O(1)
+  return result # O(1)
 
+# ----------------------
+# Post order traversal
 def post_order(root):
-    if not root:
-        return []
+   """
+    Time Complexity: O(1) -> Constant
+   """
+  if not root: # O(1)
+      return [] # O(1)
 
-    post_order(root.left)
-    post_order(root.right)
-    result.append(root.val)
-    return result
+  post_order(root.left) # O(1)
+  post_order(root.right) # O(1)
+  result.append(root.val) # O(1)
+  return result # O(1)
